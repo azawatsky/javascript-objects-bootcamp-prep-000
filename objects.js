3cca5d74b9b5fb542f24e246385d3bd3e76fd6f9
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-05-31 03:05:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'2pac':'all eyez on me'}
 
-function updatePlaylist(obj,artist,song) {
-  return Object.assign({},obj,{[artist]:song})
+function updatePlaylist(playlist,artistName,songTitle) {
+  return Object.assign({},playlist,{[artistName]:songTitle})
 }
